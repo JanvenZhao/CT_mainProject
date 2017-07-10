418879499e847b98ef601c00306887fffd9c0ad0
赵健 <zhaojian@jovision.com>
2017-07-10 16:08:24 +0800

    add main project

---
+++ CT_mainProject/CT_mainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CT_mainProject
+//
+//  Created by Janven on 17/7/10.
+//  Copyright © 2017年 Janven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CT_mainProject/CT_mainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CT_mainProject
+//
+//  Created by Janven on 17/7/10.
+//  Copyright © 2017年 Janven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
